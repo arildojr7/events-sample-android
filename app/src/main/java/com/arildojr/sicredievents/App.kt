fc87e88a378a11d53bdde48b93a992b18ab4334f
Arildo Borges Jr <sitesabj@gmail.com>
2019-09-27 13:16:46 -0300

    setup koin modules for dependency injection; add coroutines lib

---
+++ app/src/main/java/com/arildojr/sicredievents/App.kt
@@ -0,0 +1,24 @@
+package com.arildojr.sicredievents
+
+import android.app.Application
+import com.arildojr.data.di.eventModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(eventModules())
+        }
+    }
+}
