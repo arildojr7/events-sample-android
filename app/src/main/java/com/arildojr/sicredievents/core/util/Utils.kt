6697538a6274f83e85206171abe72fd6e010a49b
Arildo Borges Jr <sitesabj@gmail.com>
2019-09-29 18:05:53 -0300

    add utils fun to hide keyboard

--- app/src/main/java/com/arildojr/sicredievents/core/util/Utils.kt
+++ app/src/main/java/com/arildojr/sicredievents/core/util/Utils.kt
@@ -1,7 +1,10 @@
 package com.arildojr.sicredievents.core.util
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.arildojr.sicredievents.R
 
 fun shareContent(title: String?, description: String?, context: Context) {
@@ -25,4 +28,10 @@ fun shareContent(title: String?, description: String?, context: Context) {
             context.getString(R.string.share_with)
         )
     )
+}
+
+fun hideKeyboard(view: View) {
+    val inputMethodManager =
+        view.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
