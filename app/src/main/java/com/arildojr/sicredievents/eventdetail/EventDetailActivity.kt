5aeb2e21bcabfc1e7837b19d1d16a037f5a9c7ad
Arildo Borges Jr <sitesabj@gmail.com>
2019-09-29 08:26:04 -0300

    add `MapViewCustom` fragment to event detail screen

--- app/src/main/java/com/arildojr/sicredievents/eventdetail/EventDetailActivity.kt
+++ app/src/main/java/com/arildojr/sicredievents/eventdetail/EventDetailActivity.kt
@@ -1,26 +1,67 @@
 package com.arildojr.sicredievents.eventdetail
 
 import android.os.Bundle
+import android.view.View
+import com.arildojr.data.event.model.Event
 import com.arildojr.sicredievents.R
 import com.arildojr.sicredievents.core.base.BaseActivity
+import com.arildojr.sicredievents.core.customview.MapViewCustom
 import com.arildojr.sicredievents.databinding.ActivityEventDetailBinding
 import com.arildojr.sicredievents.eventdetail.viewmodel.EventDetailViewModel
 import com.arildojr.sicredievents.main.MainActivity.Companion.KEY_BUNDLE
 import com.arildojr.sicredievents.main.MainActivity.Companion.KEY_EVENT
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.android.viewmodel.ext.android.viewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import kotlinx.coroutines.launch
 
 class EventDetailActivity :
-    BaseActivity<ActivityEventDetailBinding>(R.layout.activity_event_detail) {
+    BaseActivity<ActivityEventDetailBinding>(R.layout.activity_event_detail), OnMapReadyCallback {
 
     private val viewModel: EventDetailViewModel by viewModel()
+
+    private val mapFragment: MapViewCustom? by lazy { supportFragmentManager.findFragmentById(R.id.map) as MapViewCustom }
+    private val event: Event? by lazy { viewModel.event }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (intent.hasExtra(KEY_BUNDLE)) {
             val bundle = intent.getBundleExtra(KEY_BUNDLE)
+            viewModel.event = bundle?.getParcelable(KEY_EVENT)
             binding.event = bundle?.getParcelable(KEY_EVENT)
+        }
 
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val latitude = event?.latitude?.toDoubleOrNull()
+        val longitude = event?.longitude?.toDoubleOrNull()
+
+        if (latitude != null && longitude != null) {
+            launch {
+                map.addMarker(
+                    MarkerOptions().position(
+                        LatLng(
+                            latitude,
+                            longitude
+                        )
+                    ).title(viewModel.event?.title)
+                )
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 11f))
+
+                mapFragment?.setListener(object : MapViewCustom.OnTouchListener {
+                    override fun onTouch() {
+                        binding.svContainer.requestDisallowInterceptTouchEvent(true)
+                    }
+                })
+            }
+        } else {
+            binding.llLocalization.visibility = View.GONE
         }
     }
 }
