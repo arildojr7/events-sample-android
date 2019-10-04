f9d9498029957694cb460544a4bd238071e9c0ad
Arildo Borges Jr <sitesabj@gmail.com>
2019-10-04 12:58:14 -0300

    refactor `EventDetailViewModel` unit tests; add id to title in main recycler view

--- app/src/test/java/com/arildojr/events/eventdetail/viewmodel/EventDetailViewModelTest.kt
+++ app/src/test/java/com/arildojr/events/eventdetail/viewmodel/EventDetailViewModelTest.kt
@@ -38,18 +38,23 @@ class EventDetailViewModelTest {
 
     @Test
     fun `check-in, when post data request, then return true if success`() {
-        val eventsLiveData = eventDetailViewModel.checkInResult
-        val observer = mock() as Observer<Boolean>
-        eventsLiveData.observeForever(observer)
+        runBlocking {
+            val eventsLiveData = eventDetailViewModel.checkInResult
+            val observer = mock() as Observer<Boolean>
+            eventsLiveData.observeForever(observer)
 
-        eventDetailViewModel.checkIn(testCheckInRequest)
+            eventDetailViewModel.checkIn(testCheckInRequest)
 
-        // verify if liveData value is true
-        verify(observer).onChanged(true)
+
+            // verify if liveData value is true
+            verify(observer).onChanged(true)
+        }
+
+
     }
 
     @Test
-    fun `validate e-mail, when send valid e-mail, then return true`() {
+    fun `validate e-mail, when send valid e-mail, then return true`() = runBlocking {
         val validEmails = listOf("sitesabj@gmail.com", "arildo@somosdx.co")
 
         validEmails.forEach { email ->
@@ -59,7 +64,7 @@ class EventDetailViewModelTest {
     }
 
     @Test
-    fun `validate e-mail, when send invalid e-mail, then return false`() {
+    fun `validate e-mail, when send invalid e-mail, then return false`() = runBlocking {
         val invalidEmails =
             listOf("sitesabj@com", "sitesabj@gmail.com.", "@somosdx.co", "somosdx.com")
 
