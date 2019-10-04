b65816a1f9ea9b1e6bec6b6112ec5d4226e461e6
Arildo Borges Jr <sitesabj@gmail.com>
2019-10-04 12:46:35 -0300

    inject `sharedViewModel` in `CheckInDialog` and implements `BaseDialogFragment`

--- app/src/main/java/com/arildojr/events/eventdetail/customview/CheckInDialog.kt
+++ app/src/main/java/com/arildojr/events/eventdetail/customview/CheckInDialog.kt
@@ -4,24 +4,21 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.arildojr.data.event.dto.CheckInUserDTO
 import com.arildojr.events.R
+import com.arildojr.events.core.base.BaseDialogFragment
 import com.arildojr.events.core.extension.clickWithDebounce
 import com.arildojr.events.core.util.hideKeyboard
 import com.arildojr.events.databinding.FragmentDialogCheckinBinding
 import com.arildojr.events.eventdetail.viewmodel.EventDetailViewModel
+import kotlinx.coroutines.launch
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
-class CheckInDialog : DialogFragment() {
+class CheckInDialog : BaseDialogFragment() {
     private lateinit var binding: FragmentDialogCheckinBinding
 
-    private val viewModel: EventDetailViewModel by lazy {
-        ViewModelProvider(requireActivity(), ViewModelProvider.NewInstanceFactory()).get(
-            EventDetailViewModel::class.java
-        )
-    }
+    private val viewModel: EventDetailViewModel by sharedViewModel()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = FragmentDialogCheckinBinding.inflate(LayoutInflater.from(context))
@@ -42,13 +39,15 @@ class CheckInDialog : DialogFragment() {
             hideKeyboard(binding.root)
             binding.isLoading = true
 
-            viewModel.checkIn(
-                CheckInUserDTO(
-                    viewModel.event?.id,
-                    binding.tieName.text.toString(),
-                    binding.tieEmail.text.toString()
+            launch {
+                viewModel.checkIn(
+                    CheckInUserDTO(
+                        viewModel.event?.id,
+                        binding.tieName.text.toString(),
+                        binding.tieEmail.text.toString()
+                    )
                 )
-            )
+            }
         }
     }
 
